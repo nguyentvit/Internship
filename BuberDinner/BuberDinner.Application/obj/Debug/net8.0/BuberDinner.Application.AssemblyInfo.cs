0763f20e5af166b39ccbfe4201e1ce279aeb8f4c
nguyentvit <nguyentv.it@gmail.com>
2024-07-04 09:10:39 +0700

    day2

--- BuberDinner/BuberDinner.Application/obj/Debug/net8.0/BuberDinner.Application.AssemblyInfo.cs
+++ BuberDinner/BuberDinner.Application/obj/Debug/net8.0/BuberDinner.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06ab4cec866403e57dd16cbd0c94aed7385bbbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+877c3ebc3d2d0204fa7e79fb43af7128cc19535b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BuberDinner/BuberDinner.Domain/obj/Debug/net8.0/BuberDinner.Domain.AssemblyInfo.cs
+++ BuberDinner/BuberDinner.Domain/obj/Debug/net8.0/BuberDinner.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06ab4cec866403e57dd16cbd0c94aed7385bbbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+877c3ebc3d2d0204fa7e79fb43af7128cc19535b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
